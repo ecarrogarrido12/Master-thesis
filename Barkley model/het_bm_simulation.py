bdbdbbe351f47a08e3d3b180f834a358be6b5e16
encarro12 <ecarrogarrido12@gmail.com>
2025-08-03 21:05:13 +0200

    Added Barkley model with heterogeneity files

---
+++ Barkley model/het_bm_simulation.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+# Numerical parameters
+N = 81
+L = 40
+n_steps = 9
+
+# Initialize the array that storages the states u(t)
+U = []; s = []
+
+# Read file
+with open("simulation_data_barkley_model.txt", "r") as file:
+    # Read heterogeneity
+    for line in file:
+        if line.startswith("#"):
+            break
+        else:
+            row = list(map(float, line.split())); s.append(row)
+    s = np.array(s)
+    # Read states
+    u_t = []
+    for line in file:
+        if line.startswith("#"):
+            u_t = np.array(u_t); U.append(u_t); u_t = []
+        else:
+            row = list(map(float, line.split())); u_t.append(row)
+    U = np.array(U)
+
+# Plot simulations
+for n in range(n_steps):
+    plt.figure(n + 1, (5, 5))
+    extent = [0, L, 0, L]
+    plt.imshow(U[n], cmap='viridis', origin='lower', extent=extent, vmin=0, vmax=1)
+    plt.xlabel('x', fontsize=18)
+    plt.ylabel('y', fontsize=18)
+    plt.savefig('het_bm_sw_'+str(n + 1)+'.png')
+
+
+# Create animation
+fig, ax = plt.subplots(figsize=(8, 4.5))
+extent = [0, L, 0, L]
+cax = ax.imshow(U[0:], cmap='viridis', origin='lower', extent=extent)
+cbar = fig.colorbar(cax)
+cbar.set_label(r'u((x, y), t)', fontsize=18)
+ax.set_xlabel('x', fontsize=18)
+ax.set_ylabel('y', fontsize=18)
+
+
+def update(frame):
+    cax.set_data(U[frame])
+    return cax,
+
+
+ani = FuncAnimation(fig, update, frames=len(U[:]), blit=False)
+ani.save('barkley_model_spiral_wave_breakup.mp4', writer='ffmpeg')
--- Barkley model/het_bm_sw.cpp
+++ Barkley model/het_bm_sw.cpp
@@ -6,20 +6,25 @@
 // Physical parameters
 const double a = 0.3;
 const double b = 0.01;
-const double eps = 1.0 / 200.0;
-const double gamma = 0.01;
+const double eps = 1.0/200.0;
+const double mu = 0.1;
 
 // Numerical parameters
-const int   L = 40;
-const int   N = 81;
+const int L = 40;
+const int N = 81;
 const double delta = 1e-4;
-const double dt = (std::pow(L, 2)) / (5 * std::pow(N - 1, 2));
+const double dt = (std::pow(L, 2))/(5*std::pow(N - 1, 2));
 const double h = static_cast<double>(L) / (static_cast<double>(N) - 1);
 const double one_m_dt = 1 - dt;
 const double one_o_a = 1.0 / a;
 const double b_o_a = b / a;
 const double dt_o_eps = dt / eps;
 const double D = dt / std::pow(h, 2);
+const double nu = dt_o_eps * mu;
+
+// Localization of the heterogeneity
+const double x_0 = L / 2;
+const double y_0 = L / 2;
 
 // Indices for the laplacian fields used in alternation
 int k = 0, kprm = 1;
@@ -37,16 +42,26 @@ void initialize_spiral_wave() {
 			if (i < N / 2) {
 				v[i][j] = a / 2.0;
 			}
-			if (j > N / 2) {
+			if (j > (N / 2)) {
 				u[i][j] = 1.0;
 			}
 		}
 	}
 }
 
-// Function that initializes the heterogeneity
-void initialize_heterogeneity() {
+// Computing the heterogeneity
+double sech(double r) {
+	return 2.0 / (std::exp(r) + std::exp(-r));
+}
 
+void initialize_heterogeneity() {
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= N; ++j) {
+			double x = i * h, y = j * h;
+			double r = std::sqrt(std::pow((x - x_0),2) + std::pow((y - y_0), 2));
+			s[i][j] = sech(r);
+		}
+	}
 }
 
 // Complete subroutine for taking one time step of the model
@@ -68,12 +83,14 @@ void step() {
 			else {
 				u_th = one_o_a * v[i][j] + b_o_a;
 				v[i][j] = v[i][j] + dt * (u[i][j] - v[i][j]);
+				// Compute the heterogeneity
+				double het = nu * s[i][j] * u[i][j]; // Recall that nu = dt_o_eps * mu
 				// Implicit form for F
 				if (u[i][j] < u_th)
-					u[i][j] = u[i][j] / (1.0 - dt_o_eps * (1.0 - u[i][j]) * (u[i][j] - u_th)) + D * lap[k][i][j];
+					u[i][j] = (u[i][j] + het) / (1.0 - dt_o_eps * (1.0 - u[i][j]) * (u[i][j] - u_th)) + D * lap[k][i][j];
 				else {
 					double temp = dt_o_eps * u[i][j] * (u[i][j] - u_th);
-					u[i][j] = (u[i][j] + temp) / (1.0 + temp) + D * lap[k][i][j];
+					u[i][j] = (u[i][j] + temp + het) / (1.0 + temp) + D * lap[k][i][j];
 				}
 				lap[kprm][i][j] = lap[kprm][i][j] - 4 * u[i][j];
 				lap[kprm][i + 1][j] = lap[kprm][i + 1][j] + u[i][j];
@@ -94,7 +111,7 @@ void step() {
 }
 
 // Function to save the state of u and v to a single file
-void save_state_to_file(std::ofstream& file) {
+void save_state_to_file(std::ofstream& file, const std::vector<std::vector<double>> & state) {
 	if (!file) {
 		std::cerr << "Error writing to file" << std::endl;
 		return;
@@ -102,7 +119,7 @@ void save_state_to_file(std::ofstream& file) {
 
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
-			file << u[i][j] << " ";
+			file << state[i][j] << " ";
 		}
 		file << "\n";
 	}
@@ -110,20 +127,24 @@ void save_state_to_file(std::ofstream& file) {
 }
 
 int main() {
+	// Open file to save all steps
+	std::ofstream file("C://hlocal//Master-thesis//Barkley model//simulation_data_barkley_model.txt");
+
 	// Initialize the spiral wave initial condition
 	initialize_spiral_wave();
 
-	// Open file to save all steps
-	std::ofstream file("simulation_data_hom_barkley_model.txt");
+	// Initialize the heterogeneity and save it in the file
+	initialize_heterogeneity();
+	save_state_to_file(file, s);
 
 	// Run the simulation for a certain number of steps and save the state
-	int num_steps = 180;
+	int num_steps = 14000;
 	for (int step_num = 0; step_num < num_steps; ++step_num) {
 		step();
 
 		// Save the state
-		if (step_num % 20 == 0) {
-			save_state_to_file(file);
+		if (step_num % 50 == 0) {
+			save_state_to_file(file, u);
 		}
 	}
 
